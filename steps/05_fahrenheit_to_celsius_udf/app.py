0eb6db5b2262765c4e0fd43561737a22e3131e21
Travis Nortell <68960601+sfc-gh-tnortell@users.noreply.github.com>
2023-02-06 15:16:56 -0800

    Changes to UDF

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -11,9 +11,14 @@
 
 import sys
 
-def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
+# Fahrenheit to C update
+# def main(temp_f: float) -> float:
+#     return (float(temp_f) - 32) * (5/9)
 
+from scipy.constants import convert_temperature
+
+def main(temp_f:float) -> float:
+        return convert_temperature(float(temp_f), 'F', 'C')
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
