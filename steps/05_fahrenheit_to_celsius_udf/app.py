d678ce1aab779732d2205311c550fb592bc4f26d
Travis Nortell <68960601+sfc-gh-tnortell@users.noreply.github.com>
2023-02-08 21:59:32 -0800

    update python function fah

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -12,13 +12,13 @@
 import sys
 
 # Fahrenheit to C update
-def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
+# def main(temp_f: float) -> float:
+#     return (float(temp_f) - 32) * (5/9)
 
-# from scipy.constants import convert_temperature
+from scipy.constants import convert_temperature
 
-# def main(temp_f:float) -> float:
-#         return convert_temperature(float(temp_f), 'F', 'C')
+def main(temp_f:float) -> float:
+        return convert_temperature(float(temp_f), 'F', 'C')
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
