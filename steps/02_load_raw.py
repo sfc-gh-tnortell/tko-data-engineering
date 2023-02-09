de5c03e1e36e11149ebd3673e071353115605104
Travis Nortell <68960601+sfc-gh-tnortell@users.noreply.github.com>
2023-02-09 13:43:18 -0800

    revert

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -38,7 +38,7 @@ def load_raw_table(session, tname=None, s3dir=None, year=None, schema=None):
 # SNOWFLAKE ADVANTAGE: Warehouse elasticity (dynamic scaling)
 
 def load_all_raw_tables(session):
-    _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XLARGE").collect()
+    _ = session.sql("ALTER WAREHOUSE HOL_WH SET WAREHOUSE_SIZE = XXLARGE").collect()
     time.sleep(5)
 
     for s3dir, data in TABLE_DICT.items():
